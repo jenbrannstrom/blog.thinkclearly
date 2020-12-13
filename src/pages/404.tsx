7b2587f345772bd7062786d4ddeb2fc5b73e836a
Aminessid <essidmohamedamin@gmail.com>
2020-12-13 22:36:06 +0100

    add sidebar

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+// import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1 className='error-msg'>404</h1>
-    <h1 className="error-msg">هذه الصفحة غير موجودة</h1>
+    {/* <SEO title="404: Not found" /> */}
+    <h1 className="error-msg">404</h1>
+    <h1 className="error-msg">PAGE NOT FOUND</h1>
   </Layout>
 )
 
