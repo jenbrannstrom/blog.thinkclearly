8a9c6f8982cbe71b31daaa6e6addd4f7fa1e0281
Aminessid <essidmohamedamin@gmail.com>
2020-12-18 12:23:17 +0100

    change the logo and google tags

--- src/html.js
+++ src/html.js
@@ -18,7 +18,7 @@ export default function HTML(props) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M9MPF9N');</script>`,
+})(window,document,'script','dataLayer','GTM-PXZZFLF');</script>`,
           }}
         />
       </head>
@@ -26,7 +26,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9MPF9N"
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXZZFLF"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
